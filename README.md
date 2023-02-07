7f1677945e894f7c42a8ec3096e15ae805881449
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-07 09:08:01 +0100

    init commit

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # grocerist-tei
 Repo for conversion and storing transcriptions from DOCX as XML/TEI files
 
+
+## usage
+
+* `scripts/download_docx.py` downloads docx from gdrive, converts it via oxgarage into an XML/TEI Document and saves it into `tmp/source.xml`
+
+## ToDo
+
+* split XML/TEI into a file for each legal-issue
+* provide proper TEI-Headers derived from [grocerist-data](https://github.com/grocerist/grocerist-data)
+
 ## Licensing
 
 All code unless otherwise noted is licensed under the terms [MIT License](https://opensource.org/licenses/MIT).
---
+++ scripts/download_docx.py
@@ -0,0 +1,42 @@
+import requests
+import shutil
+import os
+import gdown
+from acdh_tei_pyutils.tei import TeiReader
+
+CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
+TMP_DIR = "./tmp"
+SOURCE_DOC_GID = "1Dl4IPqu6OQnf1AwI03rNsx2DJDtaA2NB"
+SOURCE_DOCX_FILENAME = "source.docx"
+SOURCE_DOCX_URL = f"https://drive.google.com/uc?id={SOURCE_DOC_GID}"
+SOURCE_DOCX = os.path.join(TMP_DIR, SOURCE_DOCX_FILENAME)
+shutil.rmtree(TMP_DIR, ignore_errors=True)
+os.makedirs(TMP_DIR, exist_ok=True)
+
+print(f"start download from {SOURCE_DOCX_URL}")
+gdown.download(SOURCE_DOCX_URL, SOURCE_DOCX)
+print(f"saved {SOURCE_DOCX}")
+
+
+print(f"posting {SOURCE_DOCX} to {CONVERSION_DOMAIN}")
+headers = {
+    "accept": "application/octet-stream",
+}
+params = {
+    "properties": '<conversions><conversion index="0"><property id="oxgarage.getImages">false</property><property id="oxgarage.getOnlineImages">false</property><property id="oxgarage.lang">en</property><property id="oxgarage.textOnly">true</property><property id="pl.psnc.dl.ege.tei.profileNames">default</property></conversion></conversions>',
+}
+files = {"fileToConvert": open(SOURCE_DOCX, "rb")}
+response = requests.post(
+    f"{CONVERSION_DOMAIN}ege-webservice/Conversions/docx%3Aapplication%3Avnd.openxmlformats-officedocument.wordprocessingml.document/TEI%3Atext%3Axml",
+    params=params,
+    headers=headers,
+    files=files,
+)
+data = response.content.decode("utf-8")
+data = data.replace("heading=h.", "heading_h_")
+data = data.replace('xml:id="', 'xml:id="xmlid__')
+print(f"saving result as {SOURCE_DOCX.replace('.docx', '.xml')}")
+with open(f"{SOURCE_DOCX.replace('.docx', '.xml')}", "w") as f:
+    f.write(data)
+
+doc = TeiReader(data)
