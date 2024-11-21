a7bdff13d1c0bd7aef179e6d7f9a4ad590759ded
kisram <s2kisram@uni-trier.de>
2024-11-21 12:16:29 +0100

    paths independent of OS

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,12 @@
 import os
 
 CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
-TMP_DIR = "./tmp"
+TMP_DIR = os.path.join(".", "tmp")
 SOURCE_DOC_GID = "1Dl4IPqu6OQnf1AwI03rNsx2DJDtaA2NB"
 SOURCE_DOCX_FILENAME = "source.docx"
 SOURCE_DOCX_URL = f"https://drive.google.com/uc?id={SOURCE_DOC_GID}"
 SOURCE_DOCX = os.path.join(TMP_DIR, SOURCE_DOCX_FILENAME)
-SOURCE_XML = SOURCE_DOCX.replace('.docx', '.xml')
-TEI_DIR = "./tei"
+SOURCE_XML = SOURCE_DOCX.replace(".docx", ".xml")
+print(SOURCE_XML)
+TEI_DIR = os.path.join(".", "tei")
 DOCUMENT_URL = "https://raw.githubusercontent.com/grocerist/grocerist-data/main/json_dumps/documents.json"
