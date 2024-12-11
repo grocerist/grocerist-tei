7d7f359caf315b49f054d644d17be873dece1518
kisram <s2kisram@uni-trier.de>
2024-12-11 15:50:58 +0100

    add section divs for transcripts in case of  grocers with multiple stores

--- scripts/split.py
+++ scripts/split.py
@@ -4,9 +4,14 @@ import lxml.etree as ET
 import jinja2
 import requests
 from acdh_tei_pyutils.tei import TeiReader
+import copy
 
 
 from config import TEI_DIR, SOURCE_XML, DOCUMENT_URL
+
+nsmap = {
+    "tei": "http://www.tei-c.org/ns/1.0",
+}
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./scripts/templates")
 templateEnv = jinja2.Environment(
@@ -26,10 +31,31 @@ nsmap = doc.nsmap
 
 
 items = doc.any_xpath(".//tei:body//tei:div")
+first_pass = True
 for x in items:
     object = {}
-    doc_refs = x.xpath(".//@target[1]")
+    doc_refs = x.xpath(".//tei:head//@target", namespaces=nsmap)
+    section_refs = x.xpath(".//tei:p//@target", namespaces=nsmap)
     for ref in doc_refs:
+        if section_refs and first_pass:
+            for section in section_refs:
+                p_element = x.xpath(
+                    f"./tei:p/tei:ref[@target='{section}']", namespaces=nsmap
+                )[0].getparent()
+                parent = p_element.getparent()
+                div = ET.Element("div")
+                div.set("type", "section")
+                parent.replace(p_element, div)
+                div.append(p_element)
+                next_sibling = div.getnext()
+                while next_sibling is not None and not next_sibling.xpath(
+                    ".//@target", namespaces=nsmap
+                ):
+                    sibling_to_move = next_sibling
+                    next_sibling = div.getnext()
+                    div.append(sibling_to_move)
+            first_pass = False
+
         doc_id = ref.split("/")[-1]
         file_name = f"{doc_id}.xml"
         object["doc_id"] = doc_id
