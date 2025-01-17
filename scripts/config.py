02f61a6b4d74f91c578479845ced4b78faee1d3f
kisram <s2kisram@uni-trier.de>
2025-01-17 14:11:10 +0100

    adjust download script for multiple files

--- scripts/config.py
+++ scripts/config.py
@@ -2,11 +2,18 @@ import os
 
 CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
 TMP_DIR = os.path.join(".", "tmp")
-SOURCE_DOC_GID = "1Dl4IPqu6OQnf1AwI03rNsx2DJDtaA2NB"
-SOURCE_DOCX_FILENAME = "source.docx"
-SOURCE_DOCX_URL = f"https://drive.google.com/uc?id={SOURCE_DOC_GID}"
-SOURCE_DOCX = os.path.join(TMP_DIR, SOURCE_DOCX_FILENAME)
-SOURCE_XML = SOURCE_DOCX.replace(".docx", ".xml")
-print(SOURCE_XML)
+SOURCE_DOC_GIDS = [
+    "1Dl4IPqu6OQnf1AwI03rNsx2DJDtaA2NB",
+    "1bI8s536JXNbqIGi9v7n84Ed-4czcr9yw",
+]
+SOURCE_DOCX_FILENAMES = [f"source_{i}.docx" for i in range(len(SOURCE_DOC_GIDS))]
+SOURCE_DOCX_URLS = [f"https://drive.google.com/uc?id={gid}" for gid in SOURCE_DOC_GIDS]
+SOURCE_DOCX_FILES = [
+    os.path.join(TMP_DIR, filename) for filename in SOURCE_DOCX_FILENAMES
+]
+SOURCE_XML_FILES = [
+    docx_file.replace(".docx", ".xml") for docx_file in SOURCE_DOCX_FILES
+]
+
 TEI_DIR = os.path.join(".", "tei")
 DOCUMENT_URL = "https://raw.githubusercontent.com/grocerist/grocerist-data/main/json_dumps/documents.json"
--- scripts/download_docx.py
+++ scripts/download_docx.py
@@ -6,38 +6,40 @@ from acdh_tei_pyutils.tei import TeiReader
 from config import (
     CONVERSION_DOMAIN,
     TMP_DIR,
-    SOURCE_DOCX,
-    SOURCE_DOCX_URL,
-    SOURCE_XML
+    SOURCE_DOCX_URLS,
+    SOURCE_DOCX_FILES,
+    SOURCE_XML_FILES,
 )
 
 shutil.rmtree(TMP_DIR, ignore_errors=True)
 os.makedirs(TMP_DIR, exist_ok=True)
 
-print(f"start download from {SOURCE_DOCX_URL}")
-gdown.download(SOURCE_DOCX_URL, SOURCE_DOCX)
-print(f"saved {SOURCE_DOCX}")
+for url, docx_file, xml_file in zip(
+    SOURCE_DOCX_URLS, SOURCE_DOCX_FILES, SOURCE_XML_FILES
+):
+    print(f"start download from {url}")
+    gdown.download(url, docx_file)
+    print(f"saved {docx_file}")
 
+    print(f"posting {docx_file} to {CONVERSION_DOMAIN}")
+    headers = {
+        "accept": "application/octet-stream",
+    }
+    params = {
+        "properties": '<conversions><conversion index="0"><property id="oxgarage.getImages">false</property><property id="oxgarage.getOnlineImages">false</property><property id="oxgarage.lang">en</property><property id="oxgarage.textOnly">true</property><property id="pl.psnc.dl.ege.tei.profileNames">default</property></conversion></conversions>',  # noqa
+    }
+    files = {"fileToConvert": open(docx_file, "rb")}
+    response = requests.post(
+        f"{CONVERSION_DOMAIN}ege-webservice/Conversions/docx%3Aapplication%3Avnd.openxmlformats-officedocument.wordprocessingml.document/TEI%3Atext%3Axml",  # noqa
+        params=params,
+        headers=headers,
+        files=files,
+    )
+    data = response.content.decode("utf-8")
+    data = data.replace("heading=h.", "heading_h_")
+    data = data.replace('xml:id="', 'xml:id="xmlid__')
+    print(f"saving result as {xml_file}")
+    with open(xml_file, "w") as f:
+        f.write(data)
 
-print(f"posting {SOURCE_DOCX} to {CONVERSION_DOMAIN}")
-headers = {
-    "accept": "application/octet-stream",
-}
-params = {
-    "properties": '<conversions><conversion index="0"><property id="oxgarage.getImages">false</property><property id="oxgarage.getOnlineImages">false</property><property id="oxgarage.lang">en</property><property id="oxgarage.textOnly">true</property><property id="pl.psnc.dl.ege.tei.profileNames">default</property></conversion></conversions>', # noqa
-}
-files = {"fileToConvert": open(SOURCE_DOCX, "rb")}
-response = requests.post(
-    f"{CONVERSION_DOMAIN}ege-webservice/Conversions/docx%3Aapplication%3Avnd.openxmlformats-officedocument.wordprocessingml.document/TEI%3Atext%3Axml", # noqa
-    params=params,
-    headers=headers,
-    files=files,
-)
-data = response.content.decode("utf-8")
-data = data.replace("heading=h.", "heading_h_")
-data = data.replace('xml:id="', 'xml:id="xmlid__')
-print(f"saving result as {SOURCE_XML}")
-with open(SOURCE_XML, "w") as f:
-    f.write(data)
-
-doc = TeiReader(data)
+    doc = TeiReader(data)
