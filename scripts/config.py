d8c63beaf60a8e048c055ad23e4551803c6912ad
rausch-supola <michaela.rauschsupola@gmail.com>
2025-08-21 13:55:34 +0200

    add source link für 19th century transcript

--- scripts/config.py
+++ scripts/config.py
@@ -4,6 +4,7 @@ CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
 TMP_DIR = os.path.join(".", "tmp")
 SOURCE_DOC_GIDS = [
     "1Dl4IPqu6OQnf1AwI03rNsx2DJDtaA2NB",
+    "1RC8H43g0FOCpfNJX8n9ZA1ck4FLfl90W"
 ]
 SOURCE_DOCX_FILENAMES = [f"source_{i}.docx" for i in range(len(SOURCE_DOC_GIDS))]
 SOURCE_DOCX_URLS = [f"https://drive.google.com/uc?id={gid}" for gid in SOURCE_DOC_GIDS]
